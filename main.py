0f34c08a873accd90b919cd742fff047a7d8c9bc
vaem <vmkaaidiyatov@gmail.com>
2023-12-13 17:32:25 +0300

    added graph for alg testing

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
-from datetime import datetime
-from algorythm import greedy, lazy, explicit,bnb
+from datetime import *
+from algorythm import greedy, lazy, explicit, bnb
 from util import generator
+import matplotlib.pyplot as plt
 
 DATA_FOLDER_PATH = "data/"
 DATA_FILES = ["1.mis", "2.mis", "3.mis", "4.mis", "5.mis"]
 
 
-def print_result(n, res, start):
+def print_result(n, res, time):
     print("Минимальное вершинное покрытие: ", len(res))
     print("Размер графа: ", n)
-    print(f"Time: {datetime.now() - start}")
+    print(f"Time: {time} ms")
 
 
-def test_alg(alg):
-    for n in [10, 15, 20, 25]:
+def test_alg(alg, sizes):
+    time_results = []
+    if alg == explicit:
+        sizes = [5, 10, 20]
+    for n in sizes:
         graph = generator.generate_random_adjacency_matrix(n)
         start = datetime.now()
         res = alg.solve(graph)
-        print_result(n, res, start)
+        time = (datetime.now() - start) / timedelta(milliseconds=1)
+        print_result(n, res, time)
+        time_results.append(time)
+    if alg == explicit:
+        time_results += [0.0, 0.0, 0.0, 0.0]
+    return time_results
+
+
+def test_algs():
+    sizes = [5, 10, 20, 50, 100, 200, 500]
+    print('-' * 10, 'GREEDY ALGORYTHM TEST', '-' * 10)
+    greedy_time_results = test_alg(greedy, sizes)
+    print('-' * 10, 'BRANCH AND BOUND ALGORYTHM TEST', '-' * 10)
+    test_alg(bnb, sizes)
+    print('-' * 10, 'LAZY ALGORYTHM TEST', '-' * 10)
+    lazy_time_results = test_alg(lazy, sizes)
+    print('-' * 10, 'EXPLICIT ALGORYTHM TEST', '-' * 10)
+    explicit_time_results = test_alg(explicit, sizes)
+    
+    plt.plot(sizes, greedy_time_results, label='greedy')
+    plt.plot(sizes, lazy_time_results, label='lazy')
+    plt.plot(sizes, explicit_time_results, label='explicit')
+    plt.legend()
+    plt.show()
 
 
 def main():
-    print('-' * 10, 'GREEDY ALGORYTHM TEST', '-' * 10)
-    test_alg(greedy)
-    print('-' * 10, 'BRANCH AND BOUND ALGORYTHM TEST', '-' * 10)
-    test_alg(bnb)
-    print('-' * 10, 'EXPLICIT ALGORYTHM TEST', '-' * 10)
-    test_alg(explicit)
+    test_algs()
 
 
 if __name__ == '__main__':
